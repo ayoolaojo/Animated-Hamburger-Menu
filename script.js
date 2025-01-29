33d4ef9cad784794ca63b6999164e02089015cf2
Ayoola <johnsonayoolaojo@gmail.com>
2025-01-29 21:40:00 +0100

    files updated

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
+const side = document.getElementById("sidebar");
 
+      const hamburger = document.getElementById("hamburger");
+
+      const closeBar = document.getElementById("close-icon");
+
+      hamburger.addEventListener("click", () => {
+        side.style.display = "flex";
+      });
+
+      closeBar.addEventListener("click", () => {
+        side.style.display = "none";
+      });
 
